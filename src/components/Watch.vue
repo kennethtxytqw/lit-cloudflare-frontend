9fc514002e563963569f3d5a630f86d9679d9c65
daviddrob <david.drobnak26@gmail.com>
2021-12-12 18:29:08 +0100

    feat: list videos

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -5,7 +5,25 @@
 </template>
 
 <script>
-export default {};
+export default {
+  async mounted() {
+    const url =
+      "https://api.cloudflare.com/client/v4/accounts/9b47beba2f167662ac16b81572ee529d/stream";
+
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Auth-Email": "lightanson@protonmail.com",
+        "X-Auth-Key": "9e71cdc773da780e5059efe41ee0887d86b08",
+      },
+    };
+
+    const res = await fetch(url, options);
+    const result = await res.json();
+    console.log(result);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
